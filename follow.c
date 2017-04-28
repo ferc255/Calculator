ad586cbd5e791170ada09f0ebdfc422d864548ce
ferc255 <alekssslutskiy@yandex.ru>
2017-04-28 18:37:36 +0400

    First and Follow with ENUMs

--- follow.c
+++ follow.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define N 100
+#define N 10
 
 #define TERM_COUNT (1 << CHAR_BIT)
 
@@ -23,11 +23,9 @@ typedef enum
 	TERM_RPAREN,
 	TERM_PLUS,
 	TERM_MUL,
-	NON_TERM_E,
-	NON_TERM_E1,
-	NON_TERM_T,
-	NON_TERM_T1,
-	NON_TERM_F,
+	NON_TERM_E, // 7
+	NON_TERM_T, 
+	NON_TERM_F, // 9
 } item_t;
 
 typedef struct rule_element_t
@@ -48,16 +46,6 @@ typedef struct grammar_t
 	rule_t * rules;
 } grammar_t;
 
-grammar_t grammar = {
-	.count = 8,
-	.rules = (rule_t[]){
-    {
-		.left = NON_TERM_E,
-		.rule_size = 3,
-		.rule_elements = (rule_element_t[]) { {NON_TERM, NON_TERM_E}, {TERM, TERM_PLUS}, {NON_TERM, NON_TERM_T}, },
-    }
-  },
-};
 
 typedef struct set_t
 {
@@ -76,6 +64,82 @@ typedef struct follow_t
 	set_t follow[TERM_COUNT];
 } follow_t;
 
+typedef enum
+{
+	NOT_COUNTED,
+	IN_PROGRESS,
+	COUNTED,
+} rec_state_t;
+
+
+
+grammar_t grammar =
+{
+	.count = 6,
+	.rules = (rule_t[])
+	{
+		{
+			.left = NON_TERM_E,
+			.rule_size = 3,
+			.rule_elements = (rule_element_t[])
+			{
+				{NON_TERM, NON_TERM_E},
+				{TERM, TERM_PLUS},
+				{NON_TERM, NON_TERM_T},
+			},
+		},
+		{
+			.left = NON_TERM_E,
+			.rule_size = 1,
+			.rule_elements = (rule_element_t[])
+			{
+				{NON_TERM, NON_TERM_T},
+			},
+		},
+		{
+			.left = NON_TERM_T,
+			.rule_size = 3,
+			.rule_elements = (rule_element_t[])
+			{
+				{NON_TERM, NON_TERM_T},
+				{TERM, TERM_MUL},
+				{NON_TERM, NON_TERM_F},
+			},
+		},
+		{
+			.left = NON_TERM_T,
+			.rule_size = 1,
+			.rule_elements = (rule_element_t[])
+			{
+				{NON_TERM, NON_TERM_F},
+			},
+		},
+		{
+			.left = NON_TERM_F,
+			.rule_size = 3,
+			.rule_elements = (rule_element_t[])
+			{
+				{TERM, TERM_LPAREN},
+				{NON_TERM, NON_TERM_E},
+				{TERM, TERM_RPAREN},
+			},
+		},
+		{
+			.left = NON_TERM_F,
+			.rule_size = 1,
+			.rule_elements = (rule_element_t[])
+			{
+				{TERM, TERM_ID},
+			},
+		},
+	}
+};
+
+first_t first;
+follow_t follow;
+rec_state_t used[TERM_COUNT];
+
+
 bool contains(char arr[N][N], int size, char* token)
 {
 	int i;
@@ -89,172 +153,258 @@ bool contains(char arr[N][N], int size, char* token)
 	return false;
 }
 
-int find(char* token)
+void calcFirst(rule_element_t token)
 {
-	int i;
-	for (i = 0; i < list_size; i++)
+	if (used[token.item] == COUNTED)
 	{
-		if (strcmp(token_list[i], token) == 0)
+		return;
+	}
+	if (used[token.item] == IN_PROGRESS)
+	{
+		printf("Invalid grammar\n");
+		exit(0);
+	}
+	if (token.term_non_term == TERM)
+	{
+		first.first[token.item].list[0] = token.item;
+		first.first[token.item].mask[token.item] = true;
+		first.first[token.item].list_size = 1;
+		used[token.item] = COUNTED;
+		return;
+	}
+
+	used[token.item] = IN_PROGRESS;
+	int i;
+	for (i = 0; i < grammar.count; i++)
+	{
+		if (grammar.rules[i].left == token.item)
 		{
-			return i;
+			bool found = false;
+			int j;
+			
+			for (j = 0; j < grammar.rules[i].rule_size; j++)
+			{
+				if (grammar.rules[i].rule_elements[j].item == token.item)
+				{
+					found = true;
+					break;
+				}
+				
+				calcFirst(grammar.rules[i].rule_elements[j]);
+				item_t neigh = grammar.rules[i].rule_elements[j].item;
+				int z;
+				for (z = 0; z < first.first[neigh].list_size; z++)
+				{
+					if (first.first[neigh].list[z] != TERM_EMPTY &&
+						!first.first[token.item].
+						mask[first.first[neigh].list[z]])
+					{
+						first.first[token.item].list[first.
+							first[token.item].list_size] =
+								first.first[neigh].list[z];
+						first.first[token.item].mask[first.first[neigh].
+							list[z]] = true;
+						first.first[token.item].list_size++;
+					}
+				}
+
+				if (!first.first[neigh].mask[TERM_EMPTY])
+				{
+					found = true;
+					break;
+				}
+			}
+
+
+			if (!found && !first.first[token.item].
+				mask[TERM_EMPTY])
+			{
+				first.first[token.item].list[first.
+					first[token.item].list_size] = TERM_EMPTY;
+				first.first[token.item].mask[TERM_EMPTY] = true;
+				first.first[token.item].list_size++;
+			}				
 		}
 	}
-}
 
-void calcFirst(char* token)
-{
-	int idx = find(token);
-	if (first_ptr[idx] != 0)
-    {
-		return;
-    }
 
-	bool found = false;
-	int i;
-	for (i = 0; i < n; i++)
-    {
-		if (strcmp(gramma_head[i], token) == 0)
+
+	for (i = 0; i < grammar.count; i++)
+	{
+		if (grammar.rules[i].left == token.item)
 		{
 			int j;
-			for (j = 0; j < gramma_size[i]; j++)
+			for (j = 0; j < grammar.rules[i].rule_size; j++)
 			{
-				if (strcmp(gramma[i][j], token) == 0) // handle case then first (self) contains e
+				if (grammar.rules[i].rule_elements[j].item != token.item)
 				{
-					found = true;
-					break;
+					calcFirst(grammar.rules[i].rule_elements[j]);
 				}
-				calcFirst(gramma[i][j]); // handle case of infinite recursion 
-				int neigh = find(gramma[i][j]);
+				item_t neigh = grammar.rules[i].rule_elements[j].item;
 				int z;
-				for (z = 0; z < first_ptr[neigh]; z++)
+				for (z = 0; z < first.first[neigh].list_size; z++)
 				{
-					if (!contains(first[idx], first_ptr[idx], first[neigh][z]))
+					if (first.first[neigh].list[z] != TERM_EMPTY &&
+						!first.first[token.item].
+						mask[first.first[neigh].list[z]])
 					{
-						strcpy(first[idx][first_ptr[idx]++], first[neigh][z]);
+						first.first[token.item].list[first.
+							first[token.item].list_size] =
+								first.first[neigh].list[z];
+						first.first[token.item].mask[first.first[neigh].
+							list[z]] = true;
+						first.first[token.item].list_size++;
 					}
 				}
 
-				if (!contains(first[neigh], first_ptr[idx], "e"))
+				if (!first.first[neigh].mask[TERM_EMPTY])
 				{
-					found = true;
 					break;
 				}
 			}
-
-			if (!found && !contains(first[idx], first_ptr[idx], "e"))
-			{
-				strcpy(first[idx][first_ptr[idx]++], "e");
-			}
-		}
-    }
-
-	if (first_ptr[idx] == 0)
-    {
-		strcpy(first[idx][first_ptr[idx]++], token);
-    }
-}
-
-void calcFollow(char* token)
-{
-	int idx = find(token);
-	if (follow_ptr[idx] != 0)
-    {
-		return;
-    }
-
-	if (idx == 0)
-    {
-		strcpy(follow[idx][follow_ptr[idx]++], "$");
-    }
-
-	int i;
-	for (i = 0; i < n; i++)
-    {
-		int j;
-		for (j = 0; j < gramma_size[i]; j++)
-		{
-			if (strcmp(gramma[i][j], token) == 0)
-			{
-				int neigh;
-				if (j + 1 < gramma_size[i]) //iterate over all non-terminals which contains e
-				{
-					neigh = find(gramma[i][j + 1]);
-					int z;
-					for (z = 0; z < first_ptr[neigh]; z++)
-					{
-						if (!contains(follow[idx], follow_ptr[idx], first[neigh][z]) &&
-							strcmp(first[neigh][z], "e") != 0)
-						{
-							strcpy(follow[idx][follow_ptr[idx]++], first[neigh][z]);
-						}
-					}
-				}
-
-				if ((j + 1 == gramma_size[i] || contains(first[neigh], first_ptr[neigh], "e")) && // iterate over all following non-terminals (if all contains e in first)
-					strcmp(gramma_head[i], token) != 0)
-				{
-					calcFollow(gramma_head[i]);
-					int head = find(gramma_head[i]);
-					int z;
-					for (z = 0; z < follow_ptr[head]; z++)
-					{
-						if (!contains(follow[idx], follow_ptr[idx], follow[head][z]))
-						{
-							strcpy(follow[idx][follow_ptr[idx]++], follow[head][z]);
-						}
-					}
-				}
-			}
-		}
-    }
-}
-
-
-void add(char* token)
-{
-	int i;
-	for (i = 0; i < list_size; i++)
-	{
-		if (strcmp(token_list[i], token) == 0)
-		{
-			return;
 		}
 	}
 
-	//token_list[list_size++] = token;
-	strcpy(token_list[list_size++], token);
+	used[token.item] = COUNTED;
 }
+
+
+void calcFollow(rule_element_t token)
+{
+	if (used[token.item] == COUNTED)
+	{
+		return;
+	}
+	if (used[token.item] == IN_PROGRESS)
+	{
+		printf("Invalid grammar\n");
+		exit(0);
+	}
+	if (token.item == NON_TERM_E)
+	{
+		follow.follow[token.item].list[0] = TERM_END;
+		follow.follow[token.item].mask[TERM_END] = true;
+		follow.follow[token.item].list_size = 1;
+	}
+
+	used[token.item] = IN_PROGRESS;
+	int i;
+	for (i = 0; i < grammar.count; i++)
+	{
+		int j;
+		for (j = 0; j < grammar.rules[i].rule_size; j++)
+		{
+			if (grammar.rules[i].rule_elements[j].item == token.item)
+			{
+				bool found = false;
+				int w = 0;
+				for (w = j + 1; w < grammar.rules[i].rule_size; w++)
+				{
+					item_t neigh = grammar.rules[i].rule_elements[w].item;
+					int z;
+					for (z = 0; z < first.first[neigh].list_size; z++)
+					{
+						if (first.first[neigh].list[z] != TERM_EMPTY &&
+							!follow.follow[token.item].
+							mask[first.first[neigh].list[z]])
+						{
+							follow.follow[token.item].list[follow.
+								follow[token.item].list_size] =
+									first.first[neigh].list[z];
+							follow.follow[token.item].mask[first.
+								first[neigh].list[z]] = true;
+							follow.follow[token.item].list_size++;
+						}
+					}
+
+					if (!first.first[neigh].mask[TERM_EMPTY])
+					{
+						found = true;
+						break;
+					}							
+				}
+
+				if (!found && grammar.rules[i].left != token.item)
+				{
+					rule_element_t aux = {NON_TERM, grammar.rules[i].left};
+					calcFollow(aux);
+					item_t neigh = grammar.rules[i].left;
+					int z;
+					for (z = 0; z < follow.follow[neigh].list_size; z++)
+					{
+						if (!follow.follow[token.item].
+							mask[follow.follow[neigh].list[z]])
+						{
+							follow.follow[token.item].list[follow.
+								follow[token.item].list_size] =
+									follow.follow[neigh].list[z];
+							follow.follow[token.item].mask[follow.
+								follow[neigh].list[z]] = true;
+							follow.follow[token.item].list_size++;
+						}
+					}
+				}
+			}
+		}
+	}
+	used[token.item] = COUNTED;
+}
+
 
 int main()
-{
-	for (i = 0; i < list_size; i++)
+{	
+	int i;
+	for (i = 0; i < TERM_COUNT; i++)
 	{
-		calcFirst(i);
-	}
+		first.first[i].list_size = follow.follow[i].list_size = 0;
 
-	for (i = 0; i < list_size; i++)
-	{
-		calcFollow(token_list[i]);
-	}
-
-	for (i = 0; i < list_size; i++)
-	{
-		printf("%s\n", token_list[i]);
-		int idx = find(token_list[i]);
-
-		printf("FIRST: ");
 		int j;
-		for (j = 0; j < first_ptr[idx]; j++)
+		for (j = 0; j < TERM_COUNT; j++)
 		{
-			printf("%s ", first[idx][j]);
+			first.first[i].mask[j] = follow.follow[i].mask[j] = false;
 		}
-		printf("\n");
+	}
+	for (i = 0; i < TERM_COUNT; i++)
+	{
+		used[i] = NOT_COUNTED;
+	}
+	for (i = 0; i < grammar.count; i++)
+	{
+		int j;
+		for (j = 0; j < grammar.rules[i].rule_size; j++)
+		{
+			calcFirst(grammar.rules[i].rule_elements[j]);
+		}
+	}
 
-		printf("FOLLOW: ");
-		for (j = 0; j < follow_ptr[idx]; j++)
+	for (i = 0; i < TERM_COUNT; i++)
+	{
+		used[i] = NOT_COUNTED;
+	}
+	for (i = 0; i < grammar.count; i++)
+	{
+		int j;
+		for (j = 0; j < grammar.rules[i].rule_size; j++)
 		{
-			printf("%s ", follow[idx][j]);
+			calcFollow(grammar.rules[i].rule_elements[j]);
 		}
+	}
+	
+	for (i = 0; i < N; i++)
+	{
+		printf("%d\nFIRST: ", i);
+		int j;
+		for (j = 0; j < first.first[i].list_size; j++)
+		{
+			printf("%d ", first.first[i].list[j]);
+		}
+		
+		printf("\nFOLLOW: ");
+		for (j = 0; j < follow.follow[i].list_size; j++)
+		{
+			printf("%d ", follow.follow[i].list[j]);
+		}
+
 		printf("\n\n");
 	}
 
